65ce6697f47fb5007e7e0c2c6674becf5bc77c56
s163404 <s163404@ICS835.icsuser.nagaokaut.ac.jp>
2016-12-01 18:11:09 +0900

    DataBase完了
    テーブル作成しました。

--- WEB-INF/src/db/DataBaseProperty.java
+++ WEB-INF/src/db/DataBaseProperty.java
@@ -8,4 +8,25 @@ public class DataBaseProperty {
 	static final public String DB_USERNAME = "SeTeamA";
 
 	static final public String DB_PASSWORD = "SeTeamA";
+
+	static final public String DB_BUILDING = "ABCDEF";		//建物
+
+	static final public String DB_FLOOR = "ABCDEF";			//階
+
+	static final public String DB_NUMBER = "ABCDEF";		//部屋番号
+
+	static final public int DB_AREA = 123456;				//面積
+
+	static final public int DB_CAPACITY = 123456;			//収容人数
+
+	static final public int DB_EXTNUM = 123456;				//内線番号
+
+	static final public int DB_PROJECTOR = 123456;			//プロジェクター
+
+	static final public int DB_SCREEN = 123456;				//スクリーン
+
+	static final public int DB_WHBOARD = 123456;			//ホワイトボード
+
+	static final public int DB_INTERMINAL = 123456;			//外部入力端子
+
 }
