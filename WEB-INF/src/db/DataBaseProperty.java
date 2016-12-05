dc12b73a42804015bba03c4cdca1f92998c9c265
s163400 <s163400@ICS819.icsuser.nagaokaut.ac.jp>
2016-12-05 16:07:24 +0900

    DataBase完了
    テーブル作成しました。

--- WEB-INF/src/db/DataBaseProperty.java
+++ WEB-INF/src/db/DataBaseProperty.java
@@ -3,9 +3,9 @@ package db;
 public class DataBaseProperty {
 	static final public String DB_URL = "jdbc:sqlserver://mis.nagaokaut.ac.jp";
 
-	static final public String DB_NAME = "SeTeamA";
+	static final public String DB_NAME = "163400";
 
-	static final public String DB_USERNAME = "SeTeamA";
+	static final public String DB_USERNAME = "163400";
 
-	static final public String DB_PASSWORD = "SeTeamA";
+	static final public String DB_PASSWORD = "16340083";
 }
